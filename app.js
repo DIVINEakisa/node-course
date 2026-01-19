9b830890634da65b42945d23c33646ed57415627
DIVINEakisa <akisadivine11@gmail.com>
2026-01-19 10:00:00 +0200

    update project

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.listen(3000);
+
 app.get("/", (req, res) => {
-  res.render("index", { title: "Home" });
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
