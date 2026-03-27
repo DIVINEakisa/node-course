4f2e84a096a09d0af34bf0b79822508cadb013f7
DIVINEakisa <akisadivine11@gmail.com>
2026-03-28 01:09:00 +0200

    Add new kata

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
-app.set("view");
+
 app.listen(3000);
 app.get("/", (req, res) => {
   res.render("index");
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
-});
+
 app.use((req, res) => {
-  res.sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
