dac666cefd6ed644090a6b29693513c9a46eef8d
DIVINEakisa <akisadivine11@gmail.com>
2026-04-24 01:09:00 +0200

    Working with engines

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
+app.set("view");
 app.listen(3000);
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
